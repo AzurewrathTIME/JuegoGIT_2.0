9f7a3b33521362ca6264604b9921b8207f6aec70
AzurewrathTIME <oscarguadalupeperezverdugo@gmail.com>
2026-08-14 10:48:15 -0700

    Delete trabajo.py

--- trabajo.py
+++
@@ -1,6 +0,0 @@
-import utilidades
-if __name__ == "__main__":
-    print("Prueba de funciones implementadas en el programa, para validar su funcionamiento.")
-    print("7 es primo?:", utilidades.prime(7))
-    print("Factorial de 5:", utilidades.fac(5))
-    print("25°C en Fahrenheit:", utilidades.dCaF(25))
